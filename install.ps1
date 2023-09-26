d35f4fb0fa0ad4ff974bd81cc6eb2e0bbe93dd51
Andre Staats <andrest@bbdsoftware.com>
2023-09-26 14:57:21 +0200

    update powershell

--- install.ps1
+++ install.ps1
@@ -1,57 +1,25 @@
 #!/usr/bin/env pwsh
-# inherit from https://deno.land/x/install@v0.1.4/install.ps1
-# Copyright 2018 the Deno authors. All rights reserved. MIT license.
-
-# required:
-# 1. $repo or $r
-# 2. $version or $v
-# 2. $exe or $e
 
 $ErrorActionPreference = 'Stop'
-$inputRepo = if ($repo) {
-  "${repo}"
-} else {
-  "${r}"
-}
 
-$inputVersion = if ($version) {
-  "${version}"
-} else {
-  "${v}"
-}
+$inputRepo = if ($repo) { "${repo}" } else { "${r}" }
+$inputVersion = if ($version) { "${version}" } else { "${v}" }
+$inputExe = if ($exe) { "${exe}" } else { "${e}" }
 
-$inputExe = if ($exe) {
-  "${exe}"
-} else {
-  "${e}"
-}
-
-$githubUrl = if ($github) {
-  "${github}"
-} elseif ($g) {
-  "${g}"
-}else {
-  "https://github.com"
+if (-not (Get-Command gh -ErrorAction SilentlyContinue)) {
+    Write-Error "gh is not installed. Visit https://github.com/cli/cli#installation for installation instructions."
+    exit 1
 }
 
 $arr = $inputRepo.Split('/')
-
 $owner = $arr.Get(0)
 $repoName = $arr.Get(1)
-$exeName = "${inputExe}"
-
-if ($exeName -eq "") {
-  $exeName = "${repoName}"
-}
-
-if ($inputVersion) {
-  $version = "${inputVersion}"
-}
+$exeName = if ($inputExe) { "${inputExe}" } else { "${repoName}" }
 
 if ([Environment]::Is64BitProcess) {
-  $arch = "amd64"
+    $arch = "amd64"
 } else {
-  $arch = "386"
+    $arch = "386"
 }
 
 $BinDir = "$Home\bin"
@@ -59,50 +27,27 @@ $downloadedTagGz = "$BinDir\${exeName}.tar.gz"
 $downloadedExe = "$BinDir\${exeName}.exe"
 $Target = "windows_$arch"
 
-# GitHub requires TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$ResourceUri = if (!$version) {
-  "${githubUrl}/${owner}/${repoName}/releases/latest/download/${exeName}_${Target}.tar.gz"
-} else {
-  "${githubUrl}/${owner}/${repoName}/releases/download/${Version}/${exeName}_${Target}.tar.gz"
-}
+# Fetch asset URL using 'gh' CLI
+$ResourceUri = gh release download --repo "${owner}/${repoName}" -p "${exeName}_${Target}.tar.gz" --dir "$BinDir"
 
 if (!(Test-Path $BinDir)) {
-  New-Item $BinDir -ItemType Directory | Out-Null
-}
-
-Invoke-WebRequest $ResourceUri -OutFile $downloadedTagGz -UseBasicParsing -ErrorAction Stop
-
-function Check-Command {
-  param($Command)
-  $found = $false
-  try
-  {
-      $Command | Out-Null
-      $found = $true
-  }
-  catch [System.Management.Automation.CommandNotFoundException]
-  {
-      $found = $false
-  }
-
-  $found
+    New-Item $BinDir -ItemType Directory | Out-Null
 }
 
 if (Check-Command -Command tar) {
-  Invoke-Expression "tar -xvzf $downloadedTagGz -C $BinDir"
+    Invoke-Expression "tar -xvzf $downloadedTagGz -C $BinDir"
 } else {
-  function Expand-Tar($tarFile, $dest) {
+    function Expand-Tar {
+        param($tarFile, $dest)
 
-      if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {
-          Install-Package -Scope CurrentUser -Force 7Zip4PowerShell > $null
-      }
+        if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {
+            Install-Package -Scope CurrentUser -Force 7Zip4PowerShell > $null
+        }
 
-      Expand-7Zip $tarFile $dest
-  }
+        Expand-7Zip $tarFile $dest
+    }
 
-  Expand-Tar $downloadedTagGz $BinDir
+    Expand-Tar $downloadedTagGz $BinDir
 }
 
 Remove-Item $downloadedTagGz
@@ -110,9 +55,22 @@ Remove-Item $downloadedTagGz
 $User = [EnvironmentVariableTarget]::User
 $Path = [Environment]::GetEnvironmentVariable('Path', $User)
 if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
-  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
-  $Env:Path += ";$BinDir"
+    [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+    $Env:Path += ";$BinDir"
 }
 
 Write-Output "${exeName} was installed successfully to $downloadedExe"
 Write-Output "Run '${exeName} --help' to get started"
+
+function Check-Command {
+    param($Command)
+    $found = $false
+    try {
+        $Command | Out-Null
+        $found = $true
+    } catch [System.Management.Automation.CommandNotFoundException] {
+        $found = $false
+    }
+
+    $found
+}
